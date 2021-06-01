bfcd13c4acc85eabd59edbc471fcea1de77e9121
Temir Umurzakov <temir@umurzakov.com>
2021-06-01 08:41:24 +0600

    Genealogic tree

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,64 @@ function getGalleryImages(files) {
     }
     return images;
 }
+
+function getTreeData() {
+    var data = {
+        "Байбек": {
+            "Атай": {
+                "Чекебай": {"Саяк":null, "Курман":null},
+            },
+            "Эшбото": {
+                "Сагын": {
+                    "Омурзак": {
+                        "Кайык":{
+                            "Кыдыраалы": {"Аскар":null, "Жениш":null, "Марат":null},
+                        },
+                        "Садыбакас":{
+                            "Болот":{
+                                "Азамат": {"Арсен":null, "Дамир":null},
+                                "Темир": null,
+                            },
+                            "Мурат":{
+                                "Бермет": {"Ильгиз":null, "Ислам":null, "Алим":null},
+                                "Азиз": null
+                            },
+                            "Кубат":{ "Асель":null, "Нуриля":null },
+                        },
+                    },
+                    "Жанызак": {
+                        "Шайын": {
+                            "Суранаалы": {
+                                "Замирбек":null,
+                                "Жолдошбек": null,
+                                "Черик": null,
+                                "Венера": null,
+                                "Улан": null,
+                                "Усан" :null,
+                            }
+                        }
+                    },
+                },
+                "Аылчы": {"Абды": null},
+
+            },
+            "Муратбек": null,
+        }
+    };
+
+    var tree = getTreeNode(data);
+    return tree[0];
+}
+
+function getTreeNode(node) {
+    var children = [];
+    for(var p in node) {
+        if (node[p] == null) {
+            children.push({text: {name: p}});
+        } else if (typeof node[p] == "object") {
+            children.push({text: {name: p}, children: getTreeNode(node[p])});
+        }
+    }
+
+    return children;
+}
